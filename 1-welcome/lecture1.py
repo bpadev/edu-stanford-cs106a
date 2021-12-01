c60d3606d1c65c883e187a71ab1ff9c4996f7de7
Benjamin Arsenault <hi@benjamin.dev>
2021-11-30 19:33:42 -0500

    Basics notes

---
+++ 1-welcome/lecture1.py
@@ -0,0 +1,14 @@
+# CS106A - Lecture 1 - Code and Bit
+
+
+# Basics
+# - The computer is stupid. You must tell it exactly what to do.
+# - A computer program or "app" is a collection of code for the computer to run. 
+# - The code in a program is divided into smaller, logical units of code called functions.
+def main(a, b):
+  ...
+  ...
+  ...
+
+
+# Bit Robot - Getting Started
