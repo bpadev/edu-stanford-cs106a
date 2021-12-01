771e5e194ad6ea930cac88ac7e4edf955598816d
Benjamin Arsenault <hi@benjamin.dev>
2021-11-30 19:46:53 -0500

    Lecture 1 complete

--- 1-welcome/lecture1.py
+++ 1-welcome/lecture1.py
@@ -12,3 +12,29 @@ def main(a, b):
 
 
 # Bit Robot - Getting Started
+# - Running a function is also known as "calling" that function.
+# - Functions are usually verb like, separated by underscores. 
+def parse_data(file, format):
+  ...
+  ...
+  ...
+
+
+# Statement vs. Expression
+# - A line of Python code may be a statement or an expression.
+# - A statement runs code that performs an action.
+bit.move()
+# - An expression runs code and returns a value to that spot.
+bit.front_clear() # returns a boolean, needs to be evaluated, not telling anyone what to do.
+
+
+# While Loops
+while test-expression:
+  ...
+  ...
+  ...
+  # do this until text-expression != true
+
+
+# One Code - Many Cases - Generality
+# - General, it should work for all the different input worlds.
