9004c336e153c345a5385a0d989f1e6da432be11
Benjamin Arsenault <hi@benjamin.dev>
2021-12-01 20:31:57 -0500

    Control flow notes

---
+++ 2-control-flow/lecture2.py
@@ -0,0 +1,31 @@
+# CS106A - Lecture 2 - Control Flow
+
+
+# Basics
+# - Starts with more basic examples of the bit topic.
+# - Indentation in Python is crucial.
+# - Generality, code should work for all different input worlds. (general procedures from SICP)
+# - Talks about infinite loops and how booleans control the while loops.
+
+
+# Control Flow (If Statements)
+if test-expression:
+  ...
+  ...
+  ...
+# - If expression returns true boolean, the code in the body will execute.
+# - Python has a special value None which means "nothing".
+# - Python uses == to compare values.
+if x == 4:
+  ...
+  ...
+  ...
+# - Python uses != for not equal.
+if x != 4:
+  ...
+  ...
+  ...
+
+
+# Important Ideas - Thinking Drawing and Coding
+# - 
