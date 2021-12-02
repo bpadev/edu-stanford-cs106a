d9f0774307ce4e00ee09eeb72674ffc456a8b0af
Benjamin Arsenault <hi@benjamin.dev>
2021-12-01 21:26:59 -0500

    Lecture 3 complete

---
+++ 3-loops-decomposition/3lecture.py
@@ -0,0 +1,16 @@
+# CS106A - Lecture 3 - Loops & Decomposition
+
+
+# Decomposition - Program and Functions
+# - "Divide and Conquer", a classic strategy, works very well with computer code.
+# - Divide a program into functions, each of manageable size.
+# - Object oriented code, you call functions a little different.
+bit.left()
+# - "By name" form
+go_west(bit)
+
+
+# - Function Pre / Post Conditions
+# - Pre - precondition of function state of world required before it runs.
+# - Post - postcondition after it runs state of world promised after it's finished. 
+# - Pre / Post make the "contract" of function - what it does, what it gets to assume when it is called, what it promises to provide.
