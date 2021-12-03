4685847e39690aefa6d97ed4f00ff5380dc2f2ae
Benjamin Arsenault <hi@benjamin.dev>
2021-12-02 20:04:04 -0500

    Image intro notes

---
+++ 4-intro-to-images/lecture4.py
@@ -0,0 +1,26 @@
+# CS106A - Lecture 4 - Introduction to Images
+
+
+# Variables
+# - A Python variable has a name and stores a value.
+x = 42
+# - Think of the variable as a little box, labeled with the variable's name, containing a pointer to the value stored.
+
+
+# More About Python
+# - Python evaluates mathematical expressions following the order of operations.
+# - Python interpreter is a program on your machine that makes Python work.
+
+
+# Images
+# - Digital images are made of small square "pixels", "picture element".
+# - The color of a pixel is very often encoded as RGB.
+# - Pixels are in a x,y coordinate scheme.
+# - Origin is (0, 0) but is in the upper left.
+# - X grows right, Y grows down.
+# - Foreach loop runs over the image pixels.
+x += 4
+for x in range(10):
+  ...
+  ...
+  ...
