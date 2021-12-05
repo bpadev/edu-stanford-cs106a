30c755840795ca5864bfa41194597de3c4b2572f
Benjamin Arsenault <hi@benjamin.dev>
2021-12-04 22:58:28 -0500

    Lecture 7 complete

--- 7-functions-bools-doctests/7lecture.py
+++ 7-functions-bools-doctests/7lecture.py
@@ -3,4 +3,24 @@
 
 # Functions
 # - Each function is like a little "black box" of some computation, with data-in and data-out.
-# - 
+# - Can return a value from a function.
+def winnings1(score):
+  return score * 10
+# - None is the default return value of a function.
+# - Keep cases in mind.
+
+
+# Booleans
+# - Boolean value is True or False.
+# - Boolean operator combines values in an expression.
+and or not 
+
+
+# Strings
+# - String is a sequence of "characters" or "chars".
+# - A string written in code is called a "literal".
+len('hello') # returns number of chars in string.
+# - Strings can be empty. Important edge case to keep in mind.
+s = 'Python'
+s[0] # 'P'
+# - A string in memory is not editable (immutable).
