55e9f2505ad91f3019c71c4a4b4e7ab0c5efd779
Benjamin Arsenault <hi@benjamin.dev>
2021-12-09 19:20:55 -0500

    Complete lecture 10

---
+++ 10-strings/10lecture.py
@@ -0,0 +1,18 @@
+# CS106A - Lecture 10 - Strings
+
+
+# Strings
+# - string in, test if a substring appears in a string.
+# - Chars must match exactly.
+print('Dog' in 'CatDogBird')
+str = 'Benjamin'
+str.lower()
+# str.find(target_string)
+print(str[0:3])
+print(str[-1])
+
+
+
+# Variables
+# - The name of a variable should label that value in the code.
+# - 
