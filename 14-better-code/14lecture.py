b46e4ff862e295c435885e4fd8ace143a1913831
Benjamin Arsenault <hi@benjamin.dev>
2021-12-16 21:53:40 -0500

    Complete lecture 14

---
+++ 14-better-code/14lecture.py
@@ -0,0 +1,9 @@
+# CS106A - Lecture 14 - Better / Shorter Code
+
+
+# Unify Cases
+# - Can you combine two things into simpler form? 
+
+
+# More Functions
+# - Don't write code manually that a built-in function will do for you. 
