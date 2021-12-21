cf80fed3a28ec2034f2a543707cddc33cd3dd93b
Benjamin Arsenault <hi@benjamin.dev>
2021-12-20 19:42:17 -0500

    Complete lecture 15

--- 15-loose-ends/15lecture.py
+++ 15-loose-ends/15lecture.py
@@ -3,4 +3,12 @@
 
 # Data Parsing
 # - Have "raw" text and find and pull data you want.
-# - 
+
+
+# Loops
+# - use for i/range for the case where you know the numbers ahead of time.
+# - The while is more flexible, it can test on each iteration, stop at the right spot. 
+
+
+# Variables
+# - The variable name is just the label applied to the box that holds the pointer.
