a1613b64cea4acb9f10612f3e873ad5146cf6773
Benjamin Arsenault <hi@benjamin.dev>
2022-01-04 18:32:55 -0500

    Complete lecture 16

--- 16-drawing/16lecture.py
+++ 16-drawing/16lecture.py
@@ -2,5 +2,8 @@
 
 
 # Applied Math
-# - 
-# 
+# - Showing drawing examples on canvas
+def draw_oval(canvas, left, top, width, height):
+  canvas.fill_oval(left + 20, top + 20,      # x, y
+                    width - 40, height - 40, # w, h
+                    color='yellow')
